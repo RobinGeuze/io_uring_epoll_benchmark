51c0d0c609cce9fdea2b008617b80880b300a27b
Alex Hultman <alexhultman@gmail.com>
2020-12-07 12:05:23 +0100

    Cleanups epoll

--- epoll.c
+++ epoll.c
@@ -9,19 +9,20 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/epoll.h>
+#include <time.h>
+#include <string.h>
 
 #define MAX_PIPES 10000
+#define BUF_SIZE 1024
 
-// a vector to keep track of the pipe? no, a struct attached to both ends
+int *pipes;
+
+/* Per poll data */
 struct pipe_data {
     size_t written;
     size_t read;
     int readfd, writefd;
 };
-
-#define BUF_SIZE 1024
-
-int *pipes;
 
 void register_pipe(int epfd, int index) {
     struct pipe_data *pd = malloc(sizeof(struct pipe_data));
@@ -32,12 +33,6 @@ void register_pipe(int epfd, int index) {
     int readfd = fds[0];
     int writefd = fds[1];
 
-    //int size = BUF_SIZE;
-    //int ok = fcntl(readfd, F_SETPIPE_SZ, size);
-    //fcntl(writefd, F_SETPIPE_SZ, size);
-
-    //printf("ok: %d\n", ok);
-
     pd->readfd = readfd;
     pd->writefd = writefd;
     pd->written = 0;
@@ -45,20 +40,12 @@ void register_pipe(int epfd, int index) {
 
     pipes[index * 2 + 1] = writefd;
 
-    /* Add them to an epoll fd */
+    /* We only need to poll for readable as writing without is a known fast path */
     struct epoll_event event;
     event.events = EPOLLIN;
     event.data.ptr = pd;
     epoll_ctl(epfd, EPOLL_CTL_ADD, readfd, &event);
-
-    // we write without polling for writable (fast path)
-    //event.events = EPOLLOUT;
-    //event.data.ptr = pd;
-    //epoll_ctl(epfd, EPOLL_CTL_ADD, writefd, &event);
 }
-
-#include <time.h>
-#include <string.h>
 
 int main(int argc, char **argv) {
 
@@ -66,7 +53,6 @@ int main(int argc, char **argv) {
 
     int num_pipes = 0;
     sscanf(argv[1], "%d", &num_pipes);
-
     printf("Pipes: %d\n", num_pipes);
 
     int epfd = epoll_create1(0);
@@ -77,60 +63,64 @@ int main(int argc, char **argv) {
     /* Some shared static data */
     char *buf = malloc(BUF_SIZE);
     memset(buf, 'A', BUF_SIZE);
+
+    /* This is similar to MAX_BATCH in io_uring */
     struct epoll_event events[2000];
 
+    /* Start time */
     clock_t start = clock();
 
     /* Begin iterating, starting by writing, then writing and reading */
-    for (int i = 0; i < 10000/*00*/; i++) {
+    for (int i = 0; i < 10000; i++) {
 
         /* First we write a message to all pipes */
         for (int j = 0; j < num_pipes; j++) {
 
+            /* Pipes are ordered as readfd, writefd */
             int writefd = pipes[j * 2 + 1];
 
-            int w = write(/*pd->*/writefd, /*"Hello!"*/ buf, /*6*/ BUF_SIZE);
-            if (w != BUF_SIZE) {
-                printf("write mismatch: %d\n", w);
-                exit(1);
+            /* Write data using syscall */
+            int written = write(writefd, buf, BUF_SIZE);
+            if (written != BUF_SIZE) {
+                printf("Failed writing data in fast path: %d\n", written);
+                return 0;
             }
         }
 
-        /* Ask epoll for events */
-        int readyfd = epoll_wait(epfd, events, 2000, -1);
+        /* We expect to read everything before we continue iterating */
+        int remaining_reads = num_pipes;
 
-        /* Handle the events by either writing or reading to the pipe */
-        for (int j = 0; j < readyfd; j++) {
+        while (remaining_reads) {
+            /* Ask epoll for events in batches */
+            int readyfd = epoll_wait(epfd, events, 2000, -1);
 
-            struct pipe_data *pd = events[j].data.ptr;
+            /* Handle the events by either writing or reading to the pipe */
+            for (int j = 0; j < readyfd; j++) {
+                /* Get associated per poll data */
+                struct pipe_data *pd = events[j].data.ptr;
 
-            if (events[j].events & EPOLLIN) {
-                int r = read(pd->readfd, buf, /*6*/ BUF_SIZE);
-                if (r != BUF_SIZE) {
-                    printf("asdasdsa\n");
-                    exit(1);
+                /* If we can read, we read */
+                if (events[j].events & EPOLLIN) {
+                    int r = read(pd->readfd, buf, BUF_SIZE);
+                    if (r != BUF_SIZE) {
+                        printf("Read wrong amounts of data!\n");
+                        return 0;
+                    }
+                    pd->read += r;
                 }
-                pd->read += r;
             }
-            /*if (events[j].events & EPOLLOUT) {
-                int w = write(pd->writefd, "Hello!", 6);
-                if (w != 6) {
-                    printf("asdasdsa\n");
-                    exit(1);
-                }
-                pd->written += w;
-            }*/
+
+            /* We only keep readable fds so this is fine */
+            remaining_reads -= readyfd;
         }
     }
 
-
+    /* Print time */
     clock_t end = clock();
-
     double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-
     printf("Time: %f\n", cpu_time_used);
 
-    // append this run to epoll_runs
+    /* Append this run */
     FILE *runs = fopen("epoll_runs", "a");
     fprintf(runs, "%f\n", cpu_time_used);
     fclose(runs);
